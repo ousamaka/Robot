c4a57120651c357e19bf4e01b528c5d0f9e6daf2
Ousama Kassimi <ousamakassimi02@gmail.com>
2024-12-02 08:55:02 +0100

    Orientacion

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -2,6 +2,23 @@ package org.iesalandalus.programacion.robot.modelo;
 
 public enum Orientacion {
 
-    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE;
+    NORTE("Norte"),
+    NORESTE("Noreste"),
+    ESTE("Este"),
+    SURESTE("Sureste"),
+    SUR("Sur"),
+    SUROESTE("Suroeste"),
+    OESTE("Oeste"),
+    NOROESTE("Noroeste");
 
+    private String nombre;
+
+    Orientacion(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
