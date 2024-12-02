0d55d4ba10394cf2a1050f040061267001cff01b
Ousama Kassimi <ousamakassimi02@gmail.com>
2024-12-02 08:55:43 +0100

    zona

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
@@ -1,64 +1,51 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import java.util.Objects;
+
 public record Zona(int ancho, int alto) {
 
     public static final int ANCHO_MINIMO = 10;
     public static final int ANCHO_MAXIMO = 100;
-
     public static final int ALTO_MINIMO = 10;
     public static final int ALTO_MAXIMO = 100;
 
-
-    //canonico
+    // Constructor canónico
     public Zona {
         validarAncho(ancho);
         validarAlto(alto);
     }
 
-    //por defecto
+    // Constructor por defecto
     public Zona() {
         this(ANCHO_MINIMO, ALTO_MINIMO);
     }
 
-
-    //con atributos
-//    public Zona(int ancho, int alto) {
-//        this.ancho = ancho;
-//        this.alto = alto;
-//    }
-
-
     private void validarAncho(int ancho) {
-        if (ancho < ANCHO_MINIMO) {
-            throw new IllegalArgumentException("El valor del Ancho es menor que el mínimo permitido.");
-        } else if (ancho > ANCHO_MAXIMO) {
-            throw new IllegalArgumentException("El valor del Ancho es mayor que el mínimo permitido.");
+        if (ancho < ANCHO_MINIMO || ancho > ANCHO_MAXIMO) {
+            throw new IllegalArgumentException("Ancho no válido.");
         }
     }
 
     private void validarAlto(int alto) {
-        if (alto < ALTO_MINIMO) {
-            throw new IllegalArgumentException("El valor del Alto es menor que el mínimo permitido.");
-        } else if (alto > ALTO_MAXIMO) {
-            throw new IllegalArgumentException("El valor del Alto es mayor que el mínimo permitido.");
+        if (alto < ALTO_MINIMO || alto > ALTO_MAXIMO) {
+            throw new IllegalArgumentException("Alto no válido.");
         }
     }
 
-//    public Coordenada getCentro() {
-//        int centroX = ancho() / 2;
-//        int centroY = alto() / 2;
-//        return new Coordenada(centroX, centroY);
-//    }
+    public Coordenada getCentro() {
+        return new Coordenada(ancho / 2, alto / 2);
+    }
 
-//    public void boolean  pertenece(Coordenada coordenada) {
-//
-//    }
-//    private void perteneceX(int x) {
-//        boolean ;
-//    }
-//
-//    private void perteneceY(int y) {
-//        boolean ;
-//    }
-//
+    public boolean pertenece(Coordenada coordenada) {
+        Objects.requireNonNull(coordenada, "La coordenada no puede ser nula.");
+        return perteneceX(coordenada.x()) && perteneceY(coordenada.y());
+    }
+
+    private boolean perteneceX(int x) {
+        return x >= 0 && x < ancho;
+    }
+
+    private boolean perteneceY(int y) {
+        return y >= 0 && y < alto;
+    }
 }
